96df373479391e595f3378dd719700b3d5c423b8
Nagy Attila <nagyattila93@hotmail.com>
2014-03-21 16:23:31 +0100

    Skeleton osztály létrehozása.
    Program osztály kitisztítása.

--- src/nabukodonozor/Program.java
+++ src/nabukodonozor/Program.java
@@ -3,7 +3,6 @@ package nabukodonozor;
 public class Program {
 	public static void main(String[] args)
 	{
-		System.out.println("Nabukodonozor for the win!");
-		System.out.println("Peti vagyok!");
+		
 	}
 }
---
+++ src/nabukodonozor/Skeleton.java
@@ -0,0 +1,5 @@
+package nabukodonozor;
+
+public class Skeleton {
+
+}
