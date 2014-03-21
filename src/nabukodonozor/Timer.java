2bd2fecf1bdbeca5d9d9a77e4ce310601319ae3c
Nagy Attila <nagyattila93@hotmail.com>
2014-03-21 22:01:33 +0100

    Timer osztály elkészítése.

---
+++ src/nabukodonozor/Timer.java
@@ -0,0 +1,29 @@
+package nabukodonozor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Timer
+{
+	private List<Active> actives;
+	
+	public Timer()
+	{
+		actives = new ArrayList<Active>();
+	}
+	
+	public void tick()
+	{
+		
+	}
+	
+	public void addActive(Active a)
+	{
+		
+	}
+	
+	public void removeActive(Active a)
+	{
+		
+	}
+}
