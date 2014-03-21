d72325464c47c97b3c4d7d14936aa2a5930c2c39
Nagy Attila <nagyattila93@hotmail.com>
2014-03-21 21:20:15 +0100

    Use-case kezelő függvények felvétele a Skeleton-ban.
    Use-case-ek kilistázása a Program-ban.

--- src/nabukodonozor/Program.java
+++ src/nabukodonozor/Program.java
@@ -1,8 +1,102 @@
 package nabukodonozor;
 
+import java.util.Scanner;
+
 public class Program {	
-	public  static void main(String[] args)
+	public static void main(String[] args)
 	{
+		//a "lista"
+		String[] useCases = 	{"Initialization",
+								 "AllEnemyDies",
+								 "EnemyEntersTheGame",
+								 "EnemyStepOntoRoad",
+								 "EnemyStepOntoTrap",
+								 "EnemyGetsInRange",
+								 "EnemyReachesMountain",
+								 "TowerDamagesOrKillsEnemy",
+								 "TowerOntoRoad",
+								 "TowerOntoLand",
+								 "TowerOntoTower",
+								 "TowerOntoMountain",
+								 "TrapOntoRoad",
+								 "TrapOntoTrap",
+								 "TrapOntoLand",
+								 "TrapOntoMountain",
+								 "StoneOntoTower",
+								 "StoneOntoTrap"};
 		
+		//a lista kiírása
+		for (int i=0; i<useCases.length; i++)
+		{
+			if (i+1<10)
+			{
+				System.out.print(" ");
+			}
+			
+			System.out.println((i+1) + "\t" + useCases[i]);
+		}
+		
+		System.out.println("\nA lefuttatni kívánt use-case sorszáma: ");
+		
+		//válasz beolvasása
+		Scanner in	= new Scanner(System.in);
+		int uc		= in.nextInt();
+		in.close();
+		
+		//megfelelő függvény meghívása
+		switch (uc)
+		{
+		case  1:
+			Skeleton.uc01();
+		break;
+		case  2:
+			Skeleton.uc02();
+		break;
+		case  3:
+			Skeleton.uc03();
+		break;
+		case  4:
+			Skeleton.uc04();
+		break;
+		case  5:
+			Skeleton.uc05();
+		break;
+		case  6:
+			Skeleton.uc06();
+		break;
+		case  7:
+			Skeleton.uc07();
+		break;
+		case  8:
+			Skeleton.uc08();
+		break;
+		case  9:
+			Skeleton.uc09();
+		break;
+		case 10:
+			Skeleton.uc10();
+		break;
+		case 11:
+			Skeleton.uc11();
+		break;
+		case 12:
+			Skeleton.uc12();
+		break;
+		case 13:
+			Skeleton.uc13();
+		break;
+		case 14:
+			Skeleton.uc14();
+		break;
+		case 15:
+			Skeleton.uc15();
+		break;
+		case 16:
+			Skeleton.uc16();
+		break;
+		default:
+			System.out.println("Érvénytelen bemenet!");
+		break;
+		}
 	}
 }
--- src/nabukodonozor/Skeleton.java
+++ src/nabukodonozor/Skeleton.java
@@ -7,7 +7,7 @@ public class Skeleton {
 	//className: az objektum osztályának neve
 	//methodName: a meghívott metódus neve
 	//methodParamsID: a meghívott metódus paramétereinek azonosítói egy tömbben
-	public  static void entry(String objectName, String className, String methodName, String[] methodParamsID)
+	public static void entry(String objectName, String className, String methodName, String[] methodParamsID)
 	{
 		//tabulátor(ok) kiírása
 		for (int i=1; i<=Skeleton.tab; i++)
@@ -37,7 +37,7 @@ public class Skeleton {
 	}
 	
 	//returnValue: visszatérési érték
-	public  static void exit(String returnValue)
+	public static void exit(String returnValue)
 	{
 		Skeleton.tab--;
 		
@@ -49,5 +49,85 @@ public class Skeleton {
 		
 		//visszatérési érték kiírása
 		System.out.println("return " + returnValue);
+	}
+	
+	public static void uc01()
+	{
+		System.out.println("uc01()");
+	}
+	
+	public static void uc02()
+	{
+		System.out.println("uc02()");
+	}
+	
+	public static void uc03()
+	{
+		System.out.println("uc03()");
+	}
+	
+	public static void uc04()
+	{
+		System.out.println("uc04()");
+	}
+	
+	public static void uc05()
+	{
+		System.out.println("uc05()");
+	}
+	
+	public static void uc06()
+	{
+		System.out.println("uc06()");
+	}
+	
+	public static void uc07()
+	{
+		System.out.println("uc07()");
+	}
+	
+	public static void uc08()
+	{
+		System.out.println("uc08()");
+	}
+	
+	public static void uc09()
+	{
+		System.out.println("uc09()");
+	}
+	
+	public static void uc10()
+	{
+		System.out.println("uc10()");
+	}
+	
+	public static void uc11()
+	{
+		System.out.println("uc11()");
+	}
+	
+	public static void uc12()
+	{
+		System.out.println("uc12()");
+	}
+	
+	public static void uc13()
+	{
+		System.out.println("uc13()");
+	}
+	
+	public static void uc14()
+	{
+		System.out.println("uc14()");
+	}
+	
+	public static void uc15()
+	{
+		System.out.println("uc15()");
+	}
+	
+	public static void uc16()
+	{
+		System.out.println("uc16()");
 	}
 }
