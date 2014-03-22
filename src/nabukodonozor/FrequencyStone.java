af29456a29af012f1f41c13a6c01ae8f16e493f9
Enyedi Péter <enyedi.peter.a@gmail.com>
2014-03-22 18:01:03 +0100

    FrequencyStone osztály elkészítése

---
+++ src/nabukodonozor/FrequencyStone.java
@@ -0,0 +1,9 @@
+package nabukodonozor;
+
+public class FrequencyStone extends StoneToTower{
+
+	public void act(BasicTower b){
+		Skeleton.entry("s", "FrequencyStone", "act", new String[] {"b:BasicTower"});
+	}
+
+}
