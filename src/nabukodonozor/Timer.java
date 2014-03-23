4e78c32c8ba116a3b6e9396b4ce2997d46dec42c
Nagy Attila <nagyattila93@hotmail.com>
2014-03-24 00:04:29 +0100

    Timer kommentezése.

--- src/nabukodonozor/Timer.java
+++ src/nabukodonozor/Timer.java
@@ -4,16 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Timer {
-	private List<Active> actives;
+	private List<Active> actives; //aktiv elemek
 	
+	//konstruktor
 	public Timer() {
+		Object[] params = {};
+		Skeleton.entry(null, "Timer()", params);
+
 		actives = new ArrayList<Active>();
+		
+		Skeleton.exit("timer");
 	}
 	
+	//teendok minden utemben
 	public void tick() {
 		actives.get(0).tick();
 	}
 	
+	//aktiv elem hozzaadasa
 	public void addActive(Active a)	{
 		Object[] params = {a};
 		Skeleton.entry(this, "addActive(Active a)", params);
@@ -23,6 +31,7 @@ public class Timer {
 		Skeleton.exit("void");
 	}
 	
+	//aktiv elem eltavolitasa
 	public void removeActive(Active a) {
 		Object[] params = {a};
 		Skeleton.entry(this, "removeActive(Active a)", params);
