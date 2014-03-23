fa46644437ef27eaa533676d75bc360f36fd239d
Nagy Attila <nagyattila93@hotmail.com>
2014-03-23 23:54:36 +0100

    SpeciesSpecificBullet, Stone kommentezése.

--- src/nabukodonozor/SpeciesSpecificBullet.java
+++ src/nabukodonozor/SpeciesSpecificBullet.java
@@ -1,8 +1,15 @@
 package nabukodonozor;
 
 public interface SpeciesSpecificBullet {
+	//lovedek fejlesztese tunde-specifikusan
 	public void improveBullet(Bullet b, Elf e);
+	
+	//lovedek fejlesztese ember-specifikusan
 	public void improveBullet(Bullet b, Human h);
+	
+	//lovedek fejlesztese torp-specifikusan
 	public void improveBullet(Bullet b, Dwarf d);
+	
+	//lovedek fejlesztese hobbit-specifikusan
 	public void improveBullet(Bullet b, Hobbit h);
 }
--- src/nabukodonozor/Stone.java
+++ src/nabukodonozor/Stone.java
@@ -1,22 +1,32 @@
 package nabukodonozor;
 
-public abstract class Stone implements Incompatibility{
-
-	protected int price = 0;
-	protected int value = 0;
+public abstract class Stone implements Incompatibility {
+	protected int price = 0; //ar
+	protected int value = 0; //ertek
 	
+	//ellensegre kerulhet-e
 	public boolean accept(Enemy e){
 		Object[] params = {e};
 		Skeleton.entry(this, "accept(Enemy e)", params);
-		return false;//????
-	}
-	
-	public boolean accept(Detector d){
-		Object[] params = {d};
-		Skeleton.entry(this, "accept(Detector d)", params);
+		
+		Skeleton.exit("false");
+		
 		return false;
 	}
 	
+	//detektorra kerulhet-e
+	public boolean accept(Detector d){
+		Object[] params = {d};
+		Skeleton.entry(this, "accept(Detector e)", params);
+		
+		Skeleton.exit("false");
+		
+		return false;
+	}
+	
+	//toronyra kerulhet-e
 	public abstract boolean accept(Tower t);
+	
+	//csapdara kerulhet-e
 	public abstract boolean accept(Trap t);
 }
