0bc997c9089c8298b4d5005138b1eb1aca232d2e
Nagy Attila <nagyattila93@hotmail.com>
2014-03-24 00:16:15 +0100

    Tower kommentezése.

--- src/nabukodonozor/Tower.java
+++ src/nabukodonozor/Tower.java
@@ -4,18 +4,25 @@ import java.util.List;
 import java.util.ArrayList;
 
 public abstract class Tower extends Element implements Active {
-	protected List<StoneToTower> stones;
-	protected List<Enemy> targets;
-	protected int radius;
-	protected int frequency;
-	protected int damage;
-	protected int price;
+	protected List<StoneToTower> stones; //kovek
+	protected List<Enemy> targets; //celpontok
+	protected int radius; //hatosugar
+	protected int frequency; //lovesi gyakorisag
+	protected int damage; //sebzes
+	protected int price; //ar
 	
+	//konstruktor
 	public Tower() {
+		Object[] params = {};
+		Skeleton.entry(null, "Tower()", params);
+		
 		stones	= new ArrayList<StoneToTower>();
 		targets	= new ArrayList<Enemy>();
+		
+		Skeleton.exit("tower");
 	}
 	
+	//utra kerulhet-e
 	public boolean accept(Road r) {
 		Object[] params = {r};
 		Skeleton.entry(this, "accept(Road r)", params);
@@ -25,6 +32,7 @@ public abstract class Tower extends Element implements Active {
 		return false;
 	}
 	
+	//mezore kerulhet-e
 	public boolean accept(Land l) {
 		Object[] params = {l};
 		Skeleton.entry(this, "accept(Land l)", params);
@@ -53,6 +61,7 @@ public abstract class Tower extends Element implements Active {
 		return true;		
 	}
 	
+	//hegyre kerulhet-e
 	public boolean accept(Mountain m) {
 		Object[] params = {m};
 		Skeleton.entry(this, "accept(Mountain m)", params);
@@ -62,6 +71,7 @@ public abstract class Tower extends Element implements Active {
 		return false;
 	}
 	
+	//torony melle kerulhet-e
 	public boolean accept(Tower t) {
 		Object[] params = {t};
 		Skeleton.entry(this, "accept(Tower t)", params);
@@ -71,6 +81,7 @@ public abstract class Tower extends Element implements Active {
 		return false;
 	}
 	
+	//csapda melle kerulhet-e
 	public boolean accept(Trap t) {
 		Object[] params = {t};
 		Skeleton.entry(this, "accept(Trap t)", params);
@@ -80,6 +91,7 @@ public abstract class Tower extends Element implements Active {
 		return false;
 	}
 	
+	//ellenseg melle kerulhet-e
 	public boolean accept(Enemy e) {
 		Object[] params = {e};
 		Skeleton.entry(this, "accept(Enemy e)", params);
@@ -89,6 +101,7 @@ public abstract class Tower extends Element implements Active {
 		return false;
 	}
 	
+	//detektor melle kerulhet-e
 	public boolean accept(Detector d) {
 		Object[] params = {d};
 		Skeleton.entry(this, "accept(Detector d)", params);
@@ -98,45 +111,61 @@ public abstract class Tower extends Element implements Active {
 		return false;
 	}
 	
+	//interakcio tundevel
 	public void act(Elf e) {
 		
 	}
 	
+	//interakcio emberrel
 	public void act(Human h) {
 		
 	}
 	
+	//interakcio torppel
 	public void act(Dwarf d) {
 		
 	}
 	
+	//interakcio hobbittal
 	public void act(Hobbit h) {
 		
 	}
 	
+	//teendok minden utemben
 	public void tick() {
 		Object[] params = {};
 		Skeleton.entry(this, "tick()", params);
 		
+		//celpont kivalasztasa
 		Enemy h 	= selectTarget();
 		
+		//alap lovedek letrehozasa
 		Bullet bu	= new Bullet();
 		Skeleton.objects.put(bu, "bu:Bullet");
 		bu.increaseDamage(damage);
 
+		//lovedek fejlesztese kovekkel
 		for (StoneToTower s : stones) {
 			s.improveBullet_bridge(s, bu, h);
 		}
 		
+		//ellenseg sebzese
 		h.damage(bu);
 
 		Skeleton.exit("void");
 	}
 	
+	//ko hozzaadasa
 	public void addStone(StoneToTower s) {
+		Object[] params = {s};
+		Skeleton.entry(this, "addStone(StoneToTower s)", params);
+		
 		stones.add(s);
+		
+		Skeleton.exit("void");
 	}
 	
+	//celpont hozzaadasa
 	public void addTarget(Enemy e) {
 		Object[] params = {e};
 		Skeleton.entry(this, "addTarget(Enemy e)", params);
@@ -146,5 +175,6 @@ public abstract class Tower extends Element implements Active {
 		Skeleton.exit("void");
 	}
 	
+	//celpont kivalasztasa
 	protected abstract Enemy selectTarget();
 }
