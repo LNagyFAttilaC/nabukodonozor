c4841051c4b0ce63f4cadc2738be9c593430410b
Nagy Attila <nagyattila93@hotmail.com>
2014-03-23 23:23:47 +0100

    FrequencyStone kommentezése.

--- src/nabukodonozor/FrequencyStone.java
+++ src/nabukodonozor/FrequencyStone.java
@@ -1,10 +1,11 @@
 package nabukodonozor;
 
-public class FrequencyStone extends StoneToTower{
-
-	public void act(BasicTower b){
+public class FrequencyStone extends StoneToTower {
+	//interakció BasicTower-rel
+	public void act(BasicTower b) {
 		Object[] params = {b};
 		Skeleton.entry(this, "act(BasicTower b)", params);
+		
+		Skeleton.exit("void");
 	}
-
 }
