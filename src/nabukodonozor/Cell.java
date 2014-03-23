612df3a2f6dd35992f03f9c2701cae40b4b4425f
Savci Tamás <savcitamas@gmail.com>
2014-03-23 01:31:57 +0100

    Cell javítás

--- src/nabukodonozor/Cell.java
+++ src/nabukodonozor/Cell.java
@@ -66,13 +66,4 @@ public abstract class Cell implements Incompatibility {
 		return field;
 	}
 	
-	public Field getField(){
-		Object[] params = {};
-		Skeleton.entry(this, "getField()", params);
-		
-		Skeleton.exit("void");
-		
-		return field;
-	}
-	
 }
