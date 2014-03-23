31eba35d21c1d9eae1fbf61035d7ccd08e0736a7
Nagy Attila <nagyattila93@hotmail.com>
2014-03-23 21:26:32 +0100

    BasicTower kommentezése.

--- src/nabukodonozor/BasicTower.java
+++ src/nabukodonozor/BasicTower.java
@@ -1,12 +1,15 @@
 package nabukodonozor;
 
 public class BasicTower extends Tower {
+	//konstruktor
 	public BasicTower(){
 		Object[] params = {};
 		Skeleton.entry(this, "BasicTower()", params);
+		
 		Skeleton.exit("void");
 	}
 	
+	//celpont kivalasztasa: Kire lojon a torony a targets-bol?
 	protected Enemy selectTarget() {
 		Object[] params = {};
 		Skeleton.entry(this, "selectTarget()", params);
