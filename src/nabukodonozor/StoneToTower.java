bf010d837cd1948c1fd11b485c3d4bd10c233b77
Nagy Attila <nagyattila93@hotmail.com>
2014-03-24 00:02:15 +0100

    StoneToTower, StoneToTrap kommentezése.

--- src/nabukodonozor/StoneToTower.java
+++ src/nabukodonozor/StoneToTower.java
@@ -1,51 +1,63 @@
 package nabukodonozor;
 
 public abstract class StoneToTower extends Stone implements InteractWithTower, SpeciesSpecificBullet {
-
-	public abstract void act(BasicTower b);
-	
+	//toronyra kerulhet-e
 	public boolean accept(Tower t){
 		Object[] params = {t};
 		Skeleton.entry(this, "accept(Tower t)", params);
+		
+		Skeleton.exit("true");
 
 		return true;
 	}
 	
+	//csapdara kerulhet-e
 	public boolean accept(Trap t){
 		Object[] params = {t};
 		Skeleton.entry(this, "accept(Trap t)", params);
+		
+		Skeleton.exit("false");
 
 		return false;
 	}
 	
-	public void improveBullet(Bullet b, Elf e){
-
+	//lovedek fejlesztese tunde-specifikusan
+	public void improveBullet(Bullet b, Elf e) {
 		Object[] params = {b, e};
 		Skeleton.entry(this, "improveBullet(Bullet b, Elf e)", params);
-	}
-	
-	public void improveBullet(Bullet b, Human h){
-		Object[] params = {b, h};
-		Skeleton.entry(this, "improveBullet(Bullet b, Human h)", params);
-	}
-	
-	public void improveBullet(Bullet b, Dwarf d){
-		Object[] params = {b, d};
-		Skeleton.entry(this, "improveBullet(Bullet b, Dwarf d)", params);
-
-		
-	}
-	
-	public void improveBullet(Bullet b, Hobbit h){
-		Object[] params = {b, h};
-		Skeleton.entry(this, "improveBullet(Bullet b, Hobbit h)", params);
-
 		
 		Skeleton.exit("void");
 	}
 	
+	//lovedek fejlesztese ember-specifikusan
+	public void improveBullet(Bullet b, Human h) {
+		Object[] params = {b, h};
+		Skeleton.entry(this, "improveBullet(Bullet b, Human h)", params);
+		
+		Skeleton.exit("void");
+	}
+	
+	//lovedek fejlesztese torp-specifikusan
+	public void improveBullet(Bullet b, Dwarf d) {
+		Object[] params = {b, d};
+		Skeleton.entry(this, "improveBullet(Bullet b, Dwarf d)", params);
+		
+		Skeleton.exit("void");
+	}
+	
+	//lovedek fejlesztese hobbit-specifikusan
+	public void improveBullet(Bullet b, Hobbit h) {
+		Object[] params = {b, h};
+		Skeleton.entry(this, "improveBullet(Bullet b, Hobbit h)", params);
+		
+		Skeleton.exit("void");
+	}
+	
+	//segedmetodus
 	public void improveBullet_bridge(StoneToTower s, Bullet b, Enemy e) {
 		e.improveBullet_bridge(s, b);
-
 	}
+	
+	//interakcio BasicTower-rel
+	public abstract void act(BasicTower b);
 }
--- src/nabukodonozor/StoneToTrap.java
+++ src/nabukodonozor/StoneToTrap.java
@@ -1,28 +1,34 @@
 package nabukodonozor;
 
 public abstract class StoneToTrap extends Stone implements InteractWithTrap {
-	
-	@Override
+	//toronyra kerulhet-e
 	public boolean accept(Tower t) {
 		Object[] params = {t};
 		Skeleton.entry(this, "accept(Tower t)", params);
+		
 		Skeleton.exit("false");
+		
 		return false;
 	}
 	
-	@Override
+	//csapdara kerulhet-e
 	public boolean accept(Trap t) {
 		Object[] params = {t};
 		Skeleton.entry(this, "accept(Trap t)", params);
 		
 		t.act_bridge(this);
+		
+		//hozzaadas
 		t.setStone(this);
+		
+		//varazsero csokkentese
 		t.getCell().getField().decreaseMana(price);
 		
 		Skeleton.exit("true");
+		
 		return true;
 	}
 	
+	//interakcio BasicTrap-pel
 	public abstract void act(BasicTrap t);
-	
 }
