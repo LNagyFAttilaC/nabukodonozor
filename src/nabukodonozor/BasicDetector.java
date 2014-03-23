60982bf1ba9b897cef8b3057fd48c9fb00feb211
Nagy Attila <nagyattila93@hotmail.com>
2014-03-23 21:24:48 +0100

    BasicDetector kommentezése.

--- src/nabukodonozor/BasicDetector.java
+++ src/nabukodonozor/BasicDetector.java
@@ -1,10 +1,12 @@
 package nabukodonozor;
 
 public class BasicDetector extends Detector {
+	//default konstruktor
 	public BasicDetector() {
 		
 	}
 	
+	//a tornyot egybol csatolo konstruktor
 	public BasicDetector(Tower t) {
 		tower = t;
 	}
