7d77d03fbd599e9580aeda9915f95b4535bfdbd7
Nagy Attila <nagyattila93@hotmail.com>
2014-05-10 20:25:00 +0200

    Az ablak középre pozícionálódik. A menü kész, pálya is kiválasztható. Ez utóbbit még kiegészítem.

--- src/nabukodonozor/Game.java
+++ src/nabukodonozor/Game.java
@@ -2,9 +2,11 @@ package nabukodonozor;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -12,18 +14,12 @@ import grafikus.ImagePanel;
 
 public class Game extends JFrame {
 	private static final long serialVersionUID = 1L;
-	private JPanel menus;
-	private JPanel menu_new_game;
-	private JPanel menu_exit;
 	private JPanel field;
 	private JPanel tools;
 	private JPanel infos;
 	private JPanel icons;
 	
 	public Game() {
-		menus			= new ImagePanel("pics/menu_bg.png");
-		menu_new_game	= new ImagePanel("pics/menu_new_game.png");
-		menu_exit		= new ImagePanel("pics/menu_exit.png");
 		field			= new JPanel();
 		tools			= new JPanel();
 		infos			= new JPanel();
@@ -32,44 +28,28 @@ public class Game extends JFrame {
 		//az ablak alap beallitasai
 		getContentPane().setLayout(null);
 		setTitle("A két torony");
+		setIconImage(new ImageIcon("pics/tower.png").getImage());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setPreferredSize(new Dimension(960, 680));
 		setResizable(false);
-	}
-	
-	private void createLayout_Game() {
-		getContentPane().removeAll();
 		
-		field.setSize(960, 560);
-		field.setLocation(0, 0);
-		field.setBackground(Color.RED);
-		add(field);
-		
-		tools.setSize(300, 120);
-		tools.setLocation(  0, 560);
-		tools.setBackground(Color.GREEN);
-		add(tools);
-		
-		infos.setSize(620, 120);
-		infos.setLocation(300, 560);
-		infos.setBackground(Color.BLUE);
-		add(infos);
-		
-		icons.setSize( 40, 120);
-		icons.setLocation(920, 560);
-		icons.setBackground(Color.YELLOW);
-		add(icons);
-
-		repaint();
+		//kozepre pozicionalas
+		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+		int x = (int) ((dimension.getWidth()  - 960) / 2);
+		int y = (int) ((dimension.getHeight() - 680) / 2);
+		setLocation(x, y);
 	}
 	
 	private void createLayout_Menu() {
 		//uj jatek
+		ImagePanel menu_new_game = new ImagePanel("pics/menu_new_game.png");
 		menu_new_game.setSize(292, 44);
 		menu_new_game.setLocation(339, 353);
 		menu_new_game.addMouseListener(new MouseListener() {
 			public void mouseClicked(MouseEvent e) {
-				createLayout_Game();
+				getContentPane().removeAll();
+				createLayout_NewGame();
+				repaint();
 			}
 
 			public void mouseEntered(MouseEvent e) {}
@@ -80,6 +60,7 @@ public class Game extends JFrame {
 		add(menu_new_game);
 		
 		//kilepes
+		ImagePanel menu_exit = new ImagePanel("pics/menu_exit.png");
 		menu_exit.setSize(960, 680);
 		menu_exit.setLocation(339, 435);
 		menu_exit.addMouseListener(new MouseListener() {
@@ -95,9 +76,98 @@ public class Game extends JFrame {
 		add(menu_exit);
 		
 		//hatter
+		ImagePanel menus = new ImagePanel("pics/menu_bg.png");
 		menus.setSize(960, 680);
 		menus.setLocation(0, 0);
 		add(menus);
+	}
+	
+	private void createLayout_NewGame() {
+		//Erkezes a Fekete Kapuhoz
+		ImagePanel field1 = new ImagePanel("pics/menu_field1.png");
+		field1.setSize(292, 44);
+		field1.setLocation(339, 353);
+		field1.addMouseListener(new MouseListener() {
+			public void mouseClicked(MouseEvent e) {
+				getContentPane().removeAll();
+				createLayout_Game();
+				repaint();
+			}
+
+			public void mouseEntered(MouseEvent e) {}
+			public void mouseExited(MouseEvent e) {}
+			public void mousePressed(MouseEvent e) {}
+			public void mouseReleased(MouseEvent e) {}
+		});
+		add(field1);
+		
+		//Minath Morgul
+		ImagePanel field2 = new ImagePanel("pics/menu_field2.png");
+		field2.setSize(292, 44);
+		field2.setLocation(339, 435);
+		field2.addMouseListener(new MouseListener() {
+			public void mouseClicked(MouseEvent e) {
+				getContentPane().removeAll();
+				createLayout_Game();
+				repaint();
+			}
+
+			public void mouseEntered(MouseEvent e) {}
+			public void mouseExited(MouseEvent e) {}
+			public void mousePressed(MouseEvent e) {}
+			public void mouseReleased(MouseEvent e) {}
+		});
+		add(field2);
+		
+		//A kiralyok varosa
+		ImagePanel field3 = new ImagePanel("pics/menu_field3.png");
+		field3.setSize(292, 44);
+		field3.setLocation(339, 519);
+		field3.addMouseListener(new MouseListener() {
+			public void mouseClicked(MouseEvent e) {
+				getContentPane().removeAll();
+				createLayout_Game();
+				repaint();
+			}
+
+			public void mouseEntered(MouseEvent e) {}
+			public void mouseExited(MouseEvent e) {}
+			public void mousePressed(MouseEvent e) {}
+			public void mouseReleased(MouseEvent e) {}
+		});
+		add(field3);
+		
+		//hatter
+		ImagePanel menus = new ImagePanel("pics/menu_bg.png");
+		menus.setSize(960, 680);
+		menus.setLocation(0, 0);
+		add(menus);
+	}
+	
+	private void createLayout_Game() {
+		//palya
+		field.setSize(960, 560);
+		field.setLocation(0, 0);
+		field.setBackground(Color.RED);
+		add(field);
+		
+		//toolbox
+		tools.setSize(300, 120);
+		tools.setLocation(  0, 560);
+		tools.setBackground(Color.GREEN);
+		add(tools);
+		
+		//informaciok
+		infos.setSize(620, 120);
+		infos.setLocation(300, 560);
+		infos.setBackground(Color.BLUE);
+		add(infos);
+		
+		//ikonok
+		icons.setSize( 40, 120);
+		icons.setLocation(920, 560);
+		icons.setBackground(Color.YELLOW);
+		add(icons);
 	}
 
 	public void startGame() {
@@ -107,7 +177,5 @@ public class Game extends JFrame {
 		//megjelenites
 		pack();
 		setVisible(true);
-		
-		
 	}
 }
