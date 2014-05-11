5d8297ebd5b551d416a96e86950d4c1693d5c07e
Enyedi Péter <enyedi.peter.a@gmail.com>
2014-05-11 14:57:12 +0200

    minden jpg (majdnem)

--- src/grafikus/BasicTowerView.java
+++ src/grafikus/BasicTowerView.java
@@ -15,7 +15,7 @@ public class BasicTowerView extends TowerView{
 		int c_y = y*40;
 		
 		// kép kirajzolása
-		ImagePanel basictower = new ImagePanel("pics/tower.png");
+		ImagePanel basictower = new ImagePanel("pics/tower.jpg");
 		basictower.setSize(40, 40);
 		basictower.setLocation(c_x, c_y);
 		Program.game.getPField().add(basictower, 2);
