8fc325c10adf3d5669cccafd3979df92ffdab6ae
Savci Tamás <savcitamas@gmail.com>
2014-05-11 20:57:30 +0200

    Áttetsző bulletek

--- src/grafikus/BasicBulletView.java
+++ src/grafikus/BasicBulletView.java
@@ -9,14 +9,14 @@ public class BasicBulletView extends BulletView {
 	public BasicBulletView(BasicBullet basicBullet) {
 		bullet = basicBullet;
 	}
-	
+
 	public void notifyView() {
 		// celcella meghatarozasa - betesszuk a cella kozepere
 		int c_x = x*40 + 10;
 		int c_y = y*40 + 10;
 				
 		// kep kirajzolasa
-		ImagePanel basicBullet = new ImagePanel("pics/bullet1.jpg");
+		ImagePanel basicBullet = new ImagePanel("pics/bullet1.png");
 		basicBullet.setSize(20, 20);
 		basicBullet.setLocation(c_x, c_y);
 		Program.game.getPField().add(basicBullet, 3);		
--- src/grafikus/SlicerBulletView.java
+++ src/grafikus/SlicerBulletView.java
@@ -8,15 +8,15 @@ public class SlicerBulletView extends BulletView{
 	//konstruktor
 	public SlicerBulletView(SlicerBullet slicerBullet) {
 		bullet = slicerBullet;
-	}
-	
+	}	
+
 	public void notifyView() {
 		// celcella meghatarozasa - betesszuk a cella kozepere
 		int c_x = x*40 + 10;
 		int c_y = y*40 + 10;
 				
 		// kep kirajzolasa
-		ImagePanel basicBullet = new ImagePanel("pics/bullet2.jpg");
+		ImagePanel basicBullet = new ImagePanel("pics/bullet2.png");
 		basicBullet.setSize(20, 20);
 		basicBullet.setLocation(c_x, c_y);
 		Program.game.getPField().add(basicBullet, 3);		
