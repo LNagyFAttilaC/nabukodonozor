d7ac912cfce2595fd66a33c1d04109c74a89886c
Enyedi Péter <enyedi.peter.a@gmail.com>
2014-05-11 20:33:31 +0200

    Súgó és Fog kezdemény

--- src/grafikus/FogView.java
+++ src/grafikus/FogView.java
@@ -5,11 +5,15 @@ public class FogView extends View{
 	private Fog fog;
 	
 	public void notifyView(){
+		// célcella meghatározása
+		int c_x = x*40;
+		int c_y = y*40;
+		
 		// kép kirajzolása
-		ImagePanel land = new ImagePanel("pics/fog.jpg");
-		land.setSize(40, 40);
-		//land.setLocation(c_x, c_y);
-		Program.game.getPField().add(land);
+		ImagePanel fog = new ImagePanel("pics/fog.jpg");
+		fog.setSize(40, 40);
+		fog.setLocation(c_x, c_y);
+		Program.game.getPField().add(fog);
 	}
 	
 }
