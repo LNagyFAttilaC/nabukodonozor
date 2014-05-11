b36949dfed3d94e45a5a994775b13bbcf3383290
Enyedi Péter <enyedi.peter.a@gmail.com>
2014-05-11 11:02:15 +0200

    land kép apró módosítása, BasicTowerView elkészítése

--- src/grafikus/BasicTowerView.java
+++ src/grafikus/BasicTowerView.java
@@ -1,9 +1,20 @@
 package grafikus;
 
+import nabukodonozor.Program;
+
 public class BasicTowerView extends TowerView{
 	
 	public void notifyView(){
 		
+		// célcella meghatározása
+		int c_x = x*40;
+		int c_y = y*40;
+		
+		// kép kirajzolása
+		ImagePanel basictower = new ImagePanel("pics/tower.png");
+		basictower.setSize(40, 40);
+		basictower.setLocation(c_x, c_y);
+		Program.game.getPField().add(basictower);
 	}
 
 }
