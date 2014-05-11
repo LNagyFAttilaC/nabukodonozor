bc5116dc48ab0afd8e0d04898a5d8d2f04252817
Enyedi Péter <enyedi.peter.a@gmail.com>
2014-05-11 14:00:35 +0200

    Fog elkezdése

--- src/grafikus/FogView.java
+++ src/grafikus/FogView.java
@@ -5,7 +5,11 @@ public class FogView extends View{
 	private Fog fog;
 	
 	public void notifyView(){
-		
+		// kép kirajzolása
+		ImagePanel land = new ImagePanel("pics/fog.jpg");
+		land.setSize(40, 40);
+		//land.setLocation(c_x, c_y);
+		Program.game.getPField().add(land);
 	}
 	
 }
