d0a81860e4585881a9633300504a3ab90eb1793e
Nagy Attila <nagyattila93@hotmail.com>
2014-05-11 02:06:18 +0200

    Javítás.

--- src/grafikus/MountainView.java
+++ src/grafikus/MountainView.java
@@ -1,15 +1,24 @@
 package grafikus;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
+import nabukodonozor.Field;
 import nabukodonozor.Mountain;
+import nabukodonozor.Program;
 
-public class MountainView extends CellView{
+public class MountainView extends CellView {
 	//konstruktor
 	public MountainView(Mountain mountain) {
 		cell = mountain;
@@ -17,17 +26,13 @@ public class MountainView extends CellView{
 	
 	public void notifyView(){
 		// célcella meghatározása
-		
+		int c_x = x*40;
+		int c_y = y*40;
 		
 		// kép kirajzolása
-		BufferedImage img;
-		try {
-			img = ImageIO.read(new File("pics/mountain.jpg"));
-			ImageIcon icon=new ImageIcon(img);
-			/*Graphics g;
-			g.drawImage(null, coords[0], coords[1], null);*/
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		ImagePanel land = new ImagePanel("pics/mountain.jpg");
+		land.setSize(40, 40);
+		land.setLocation(200, 400);
+		Program.game.add(land);
 	}
 }
