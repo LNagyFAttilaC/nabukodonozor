8adfdf099f3807e386558f343beaf0d148cb7e62
Nagy Attila <nagyattila93@hotmail.com>
2014-05-12 05:15:59 +0200

    Kövek hozzáadása.

--- src/grafikus/Controller.java
+++ src/grafikus/Controller.java
@@ -6,7 +6,13 @@ import nabukodonozor.BasicTower;
 import nabukodonozor.BasicTrap;
 import nabukodonozor.Cell;
 import nabukodonozor.DamageStone;
+import nabukodonozor.DwarfDamageStone;
+import nabukodonozor.ElfDamageStone;
+import nabukodonozor.FrequencyStone;
+import nabukodonozor.HobbitDamageStone;
+import nabukodonozor.HumanDamageStone;
 import nabukodonozor.Program;
+import nabukodonozor.RadiusStone;
 import nabukodonozor.Tower;
 
 public class Controller {
@@ -72,12 +78,78 @@ public class Controller {
 		break;
 		case DAMAGESTONE_CLICKED:
 			if (Program.game.getField().getMana() >= DamageStone.default_price) {
-				DamageStone damagestone = new DamageStone();
+				DamageStone stone = new DamageStone();
 				
 				Tower tower = cell.getTower();
 				
 				if (tower != null) {
-					tower.addStone(damagestone);
+					tower.addStone(stone);
+				}
+			}
+		break;
+		case RADIUSSTONE_CLICKED:
+			if (Program.game.getField().getMana() >= RadiusStone.default_price) {
+				RadiusStone stone = new RadiusStone();
+				
+				Tower tower = cell.getTower();
+				
+				if (tower != null) {
+					tower.addStone(stone);
+				}
+			}
+		break;
+		case ELFDAMAGESTONE_CLICKED:
+			if (Program.game.getField().getMana() >= ElfDamageStone.default_price) {
+				ElfDamageStone stone = new ElfDamageStone();
+				
+				Tower tower = cell.getTower();
+				
+				if (tower != null) {
+					tower.addStone(stone);
+				}
+			}
+		break;
+		case HUMANDAMAGESTONE_CLICKED:
+			if (Program.game.getField().getMana() >= HumanDamageStone.default_price) {
+				HumanDamageStone stone = new HumanDamageStone();
+				
+				Tower tower = cell.getTower();
+				
+				if (tower != null) {
+					tower.addStone(stone);
+				}
+			}
+		break;
+		case FREQUENCYSTONE_CLICKED:
+			if (Program.game.getField().getMana() >= FrequencyStone.default_price) {
+				FrequencyStone stone = new FrequencyStone();
+				
+				Tower tower = cell.getTower();
+				
+				if (tower != null) {
+					tower.addStone(stone);
+				}
+			}
+		break;
+		case HOBBITDAMAGESTONE_CLICKED:
+			if (Program.game.getField().getMana() >= HobbitDamageStone.default_price) {
+				HobbitDamageStone stone = new HobbitDamageStone();
+				
+				Tower tower = cell.getTower();
+				
+				if (tower != null) {
+					tower.addStone(stone);
+				}
+			}
+		break;
+		case DWARFDAMAGESTONE_CLICKED:
+			if (Program.game.getField().getMana() >= DwarfDamageStone.default_price) {
+				DwarfDamageStone stone = new DwarfDamageStone();
+				
+				Tower tower = cell.getTower();
+				
+				if (tower != null) {
+					tower.addStone(stone);
 				}
 			}
 		break;
