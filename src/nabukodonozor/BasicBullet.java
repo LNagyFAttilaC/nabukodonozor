fd4b2032525242f9d82d831269282d0a60083d4d
Savci Tamás <savcitamas@gmail.com>
2014-05-12 02:42:01 +0200

    Torony lövése grafika (még elég gagyi, de már megvan a lényeg, csak nem szép)

--- src/nabukodonozor/BasicBullet.java
+++ src/nabukodonozor/BasicBullet.java
@@ -1,8 +1,26 @@
 package nabukodonozor;
 
+import grafikus.BasicBulletView;
+
 public class BasicBullet extends Bullet {
+	//konstruktor
+	public BasicBullet(Tower from) {
+		super();
+		view = new BasicBulletView(this);
+		int[] cell_coords = from.getCell().getView().getCoords();
+		view.setCoords(cell_coords[0], cell_coords[1]);
+	}
+	
 	//interakcio az ellenseggel
 	public void act(Enemy e) {
+		drawable = false;
+		view.notifyView();
+		int[] cell_coords = e.getCell().getView().getCoords();
+		view.setCoords(cell_coords[0], cell_coords[1]);
 		e.decreaseLife(damage);
+		drawable = true;
+		view.notifyView();
+		drawable = false;
+		view.notifyView();
 	}
 }
